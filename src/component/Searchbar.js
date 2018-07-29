a73727d0e71d19ab49248a275d909b2781e4005c
Pheno <kashani.public@gmail.com>
2018-07-29 11:56:37 +0300

    Searchbar added

---
+++ src/component/Searchbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Searchbar = (props) => {
+    const {locations, filteredLocations, selectedLocation, selectLocation, queryUpdate} = props;
+    let locs = filteredLocations === [] ? locations : filteredLocations;
+    return (
+        <div className='search-and-list'>
+            <div>
+                <input type="text"
+                    className='search-box'
+                    placeholder='Restaurant Name'
+                    aria-label='Search Restaurant'
+                    onChange={event => queryUpdate(event.target.value)}/>
+            </div>
+            <ul>
+                {
+                    locs.map((location, index) => {
+                        const isSelected = (location.id === selectedLocation.id ? 'row-selected' : '');
+                        return (
+                            <li 
+                                key={index}
+                                onClick={ () => selectLocation(location)}
+                                onKeyDown={(event) => event.keyCode !== 13 || selectLocation(location)}
+                                className={isSelected}
+                                tabIndex={0}
+                            >
+                                <span>{location.name}</span>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+    );
+}
+
+export default Searchbar
